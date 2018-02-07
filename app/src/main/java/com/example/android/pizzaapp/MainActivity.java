0697db99a15912f490873999fc649d65c01a478f
josetedd <josetedd@gmail.com>
2018-02-07 17:13:07 +0300

    Added check user in MainActivity.java commented out name EditText in activity_login.xml removed onclick attributes on Button and TextView that was making app to crash changed text in Button and TextView

--- app/src/main/java/com/example/android/pizzaapp/MainActivity.java
+++ app/src/main/java/com/example/android/pizzaapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.android.pizzaapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    //declare FirebaseAuth instance
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize firebaseAuth instance
         mAuth = FirebaseAuth.getInstance();
+        //check if user is already logged in using getCurrentUser()
+        if (mAuth.getCurrentUser() == null) {
+            //if user is not logged in, close this activity
+            finish();
+            // and open the login activity
+            startActivity(new Intent(getApplicationContext(), loginActivity.class));
+        }
+
     }
 }
