9160b7e576d4ff9eb9fb0369a70cee41f062ae36
josetedd <josetedd@gmail.com>
2018-02-15 16:01:27 +0300

    Added Password Reset. Users can now request for password reset

---
+++ app/src/main/java/com/example/android/pizzaapp/ResetPassActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.pizzaapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPassActivity extends AppCompatActivity {
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    EditText emailEdit;
+    Button resetBtn;
+    Button backBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass);
+        // Initialize views
+        emailEdit = findViewById(R.id.inputemailReset);
+        resetBtn = findViewById(R.id.ResetBtn);
+        backBtn = findViewById(R.id.backBtn);
+
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    public void resetPass(){
+        String email = emailEdit.getText().toString().trim();
+        // progressdialog initialize
+        progressDialog = new ProgressDialog(ResetPassActivity.this);
+        progressDialog.setMessage("Resetting..."); // Setting Message
+        progressDialog.setTitle("Reset"); // Setting Title
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER); // Progress Dialog Style Spinner
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"No email entered", Toast.LENGTH_LONG);
+            return;
+        }
+
+        //Reset password
+        progressDialog.show();// display progress dialog
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ResetPassActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ResetPassActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+    public void onClick(View view){
+        if(view == resetBtn){
+           resetPass();
+        }
+        if (view == backBtn){
+           finish();
+            startActivity(new Intent(getApplicationContext(), loginActivity.class));
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/android/pizzaapp/loginActivity.java
+++ app/src/main/java/com/example/android/pizzaapp/loginActivity.java
@@ -7,7 +7,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +22,9 @@ public class loginActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     EditText emailEdit;
     EditText passwordEdit;
+    Button loginBtn;
+    TextView resetLink;
+    TextView signupLink;
 
 
     @Override
@@ -29,20 +34,24 @@ public class loginActivity extends AppCompatActivity {
         //Initialize firebaseAuth instance
         mAuth = FirebaseAuth.getInstance();
         // if user is already logged in start main activity
-        if(mAuth.getCurrentUser() !=null){
+        if (mAuth.getCurrentUser() != null) {
             finish();
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
         }
         // initialize views
         emailEdit = findViewById(R.id.inputemail);
         passwordEdit = findViewById(R.id.inputpassword);
+        loginBtn = findViewById(R.id.loginbutton);
+        resetLink = findViewById(R.id.resetLink);
+        signupLink = findViewById(R.id.signinlink);
 
     }
+
     //
-    public void login(){
+    public void login() {
         //getting email and password from edit texts
         String email = emailEdit.getText().toString().trim();
-        String password  = passwordEdit.getText().toString().trim();
+        String password = passwordEdit.getText().toString().trim();
         // progressdialog initialize
         progressDialog = new ProgressDialog(loginActivity.this);
         progressDialog.setMessage("logging in..."); // Setting Message
@@ -51,13 +60,13 @@ public class loginActivity extends AppCompatActivity {
 
 
         //checking if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -68,30 +77,32 @@ public class loginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //checking if success
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             finish();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }else{
+                        } else {
                             //display some message here
-                            Toast.makeText(loginActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
+                            Toast.makeText(loginActivity.this, "Registration Error", Toast.LENGTH_LONG).show();
                         }
                         progressDialog.dismiss();
                     }
                 });
 
 
-
     }
+
     //
-    public void onClick(View view){
-        login();
+    public void onClick(View view) {
+        if(view ==loginBtn) {
+            login();
+        }
+        if(view == resetLink){
+            startActivity(new Intent(getApplicationContext(), ResetPassActivity.class));
 
-    }
-
-    // method to open SignUpActivity
-    public void openSignupActivity(View view) {
-
-        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        }
+        if(view == signupLink){
+            startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        }
 
     }
 }
