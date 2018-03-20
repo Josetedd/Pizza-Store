ceaddf16541e06d66bd12994459f967c563046cf
Brian Otieno <brianoti44@gmail.com>
2018-03-20 18:44:02 +0300

    Fixed quantity minus buttons in Ordered_activity

--- app/src/main/java/com/example/android/pizzaapp/Ordered_activity.java
+++ app/src/main/java/com/example/android/pizzaapp/Ordered_activity.java
@@ -1,7 +1,7 @@
 package com.example.android.pizzaapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,33 +9,37 @@ import android.widget.Toast;
 public class Ordered_activity extends AppCompatActivity {
 
     //Tracks the quantity for items One ,Two and Three
-    int qtyItemOne , qtyItemTwo , qtyItemThree= 0; // declaration
+    int qtyItemOne = 1, qtyItemTwo = 1, qtyItemThree = 1; // declaration
+    int priceItemOne = 355, priceItemTwo = 1, priceItemThree = 1; // declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordered_activity);
     }
+
     /**
      * Decrease the quantity for Item One by 1.
      */
-    public void minusOneforItemOne(View v){
-        if (qtyItemOne == 1){
+    public void minusOneforItemOne(View v) {
+        if (qtyItemOne >= 2) {
+            qtyItemOne = qtyItemOne - 1;
+            displayForItemOne(qtyItemOne);
+        } else {
             //show an error message as a toast
-            Toast.makeText(this,"You can't have less than 1 pizza",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You can't have less than 1 pizza", Toast.LENGTH_SHORT).show();
             //Exit this method early because there's nothing left to do
-            return;
         }
-        qtyItemOne = qtyItemOne - 1;
-        displayForItemOne(qtyItemOne);
     }
+
     /**
-     * Increase the quantity for Item One by 1.
+     * Increase the quantity for Ite One by 1.
      */
-    public void addOneforItemOne(View v){
+    public void addOneforItemOne(View v) {
         qtyItemOne = qtyItemOne + 1;
         displayForItemOne(qtyItemOne);
     }
+
     /**
      * Displays the quantity for item One.
      */
@@ -43,26 +47,29 @@ public class Ordered_activity extends AppCompatActivity {
         TextView quantityView = (TextView) findViewById(R.id.qty1);
         quantityView.setText(String.valueOf(quantity));
     }
+
     /**
      * Decrease the quantity for Item Two by 1.
      */
-    public void minusOneforItemTwo(View v){
-        if (qtyItemTwo == 1){
+    public void minusOneforItemTwo(View v) {
+        if (qtyItemTwo >= 2) {
+            qtyItemTwo = qtyItemTwo - 1;
+            displayForItemOne(qtyItemTwo);
+        } else {
             //show an error message as a toast
-            Toast.makeText(this,"You can't have less than 1 pizza",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You can't have less than 1 pizza", Toast.LENGTH_SHORT).show();
             //Exit this method early because there's nothing left to do
-            return;
         }
-        qtyItemTwo = qtyItemTwo - 1;
-        displayForItemTwo(qtyItemTwo);
     }
+
     /**
      * Increase the quantity for Item One by 1.
      */
-    public void addOneforItemTwo(View v){
+    public void addOneforItemTwo(View v) {
         qtyItemTwo = qtyItemTwo + 1;
         displayForItemTwo(qtyItemTwo);
     }
+
     /**
      * Displays the quantity for item Two.
      */
@@ -70,26 +77,29 @@ public class Ordered_activity extends AppCompatActivity {
         TextView quantityView = (TextView) findViewById(R.id.qty2);
         quantityView.setText(String.valueOf(quantity));
     }
+
     /**
      * Decrease the quantity for Item Three by 1.
      */
-    public void minusOneforItemThree(View v){
-        if (qtyItemThree == 1){
+    public void minusOneforItemThree(View v) {
+        if (qtyItemThree >= 2) {
+            qtyItemThree = qtyItemThree - 1;
+            displayForItemOne(qtyItemThree);
+        } else {
             //show an error message as a toast
-            Toast.makeText(this,"You can't have less than 1 pizza",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You can't have less than 1 pizza", Toast.LENGTH_SHORT).show();
             //Exit this method early because there's nothing left to do
-            return;
         }
-        qtyItemThree = qtyItemThree - 1;
-        displayForItemThree(qtyItemThree);
     }
+
     /**
      * Increase the quantity for Item One by 1.
      */
-    public void addOneforItemThree(View v){
+    public void addOneforItemThree(View v) {
         qtyItemThree = qtyItemThree + 1;
         displayForItemThree(qtyItemThree);
     }
+
     /**
      * Displays the quantity for item Three.
      */
